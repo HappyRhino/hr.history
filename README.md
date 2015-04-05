3b0e260d43e3bd6c71ce83eb91a440a822e1c1ef
Samy Pessé <samypesse@gmail.com>
2015-04-05 19:10:33 +0200

    Complete code and tests

--- README.md
+++ README.md
@@ -11,3 +11,25 @@ $ npm install hr.history
 
 ## Documentation
 
+#### Create a router
+
+```js
+var history = require("hr.history");
+
+var router = new history.Router({
+    routes: {
+        "post/:id": function(postId) {
+            // Do something with the post id
+        }
+    }
+});
+
+// Start handling history
+router.start();
+```
+
+#### Navigate
+
+```js
+router.navigate("post/:id", { id: "hello-world" });
+```
--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,33 @@
+var history = require("../");
 
 describe('History', function() {
-
+    it('should correctly trigger "change" event', function(done) {
+        history.start();
+        history.once('change', function(url) {
+            expect(url).to.equal("hello/world");
+            done();
+        });
+        window.location.hash = "/hello/world";
+    });
 });
 
 describe('Router', function() {
+
+    it('should correctly trigger route', function(done) {
+        var router = new history.Router({
+            routes: {
+                "post/:id": function(postId) {
+                    expect(postId).to.equal("hello-world");
+                    done();
+                }
+            }
+        });
+
+        // Start handling history
+        router.start();
+
+        window.location.hash = "/post/hello-world";
+    });
 
 });
 
